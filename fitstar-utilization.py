b8c52242604c8cf6f29cacd66c2fe45c4b897f26
Sandro-Alessio Gierens <sandro@gierens.de>
2022-06-20 01:11:06 +0200

    Add finding the studio list

--- fitstar-utilization.py
+++ fitstar-utilization.py
@@ -84,6 +84,11 @@ def main():
     studios_downdown_trigger.click()
     print('open studios dropdown')
 
+    # find studio list
+    studios_row: WebElement = browser.find_element_by_xpath(
+        "//ul[contains(@class, 'row-studios')]")
+    print('found studio list')
+
     close_browser(browser)
 
 
